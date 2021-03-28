788dac399fb4e0a85d509d9d11b73bf804f15d10
Pavel.Sergeev <pavel.sergeev@jetbrains.com>
2021-03-29 02:21:57 +0300

    setup sandbox

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,12 @@
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
-import ru.tinkoff.invest.openapi.model.rest.*
+import ru.tinkoff.invest.openapi.model.rest.SandboxCurrency
+import ru.tinkoff.invest.openapi.model.rest.SandboxRegisterRequest
+import ru.tinkoff.invest.openapi.model.rest.SandboxSetCurrencyBalanceRequest
+import ru.tinkoff.invest.openapi.model.rest.SandboxSetPositionBalanceRequest
 import ru.tinkoff.invest.openapi.model.streaming.CandleInterval
 import ru.tinkoff.invest.openapi.model.streaming.StreamingRequest
 import ru.tinkoff.invest.openapi.okhttp.OkHttpOpenApi
@@ -30,55 +33,45 @@ private fun readToken(): String? {
     return classLoader.getResourceAsStream("token")?.bufferedReader()?.readText()
 }
 
-suspend fun main()  {
+fun main() = runBlocking {
     initLogger()
     val logger = LoggerFactory.getLogger("main")
-    val token = readToken()!!
+    val token = readToken() ?: throw Error("Place your token in resources/token")
     val sandboxMode = true
     val ticker = "AAPL"
     val candleInterval = CandleInterval._1MIN
+
     OkHttpOpenApi(token, sandboxMode).use { api ->
+        val instrumentsList = api.marketContext.searchMarketInstrumentsByTicker(ticker).join()
+        val instrument = instrumentsList.instruments.stream().findFirst().get()
+
         if (api.isSandboxMode) {
             api.sandboxContext.performRegistration(SandboxRegisterRequest()).join()
+            api.sandboxContext.clearAll(null)
+            api.sandboxContext.setCurrencyBalance(SandboxSetCurrencyBalanceRequest().apply {
+                currency = SandboxCurrency.USD
+                balance = 1000.0.toBigDecimal()
+            }, null)
+            api.sandboxContext.setPositionBalance(SandboxSetPositionBalanceRequest().apply {
+                figi = instrument.figi
+                balance = 10.toBigDecimal()
+            }, null)
+        }
+        launch {
+            logger.info("Run events consumer")
+            api.streamingContext.asFlow().collect { event ->
+                logger.info("Got event: $event")
+            }
         }
 
         val currentOrders = api.ordersContext.getOrders(null).join()
         logger.info("Current orders: $currentOrders")
-
         val currentPositions = api.portfolioContext.getPortfolio(null).join()
         logger.info("Current positions: $currentPositions")
-
-        val instrumentsList = api.marketContext.searchMarketInstrumentsByTicker(ticker).join()
-        val instrument = instrumentsList.instruments.stream().findFirst().get()
-        logger.info("Instrument: $instrument")
 
         val portfolioCurrencies = api.portfolioContext.getPortfolioCurrencies(null).join()
         logger.info("Portfolio currency: $portfolioCurrencies")
 
         api.streamingContext.sendRequest(StreamingRequest.subscribeCandle(instrument.figi, candleInterval))
-        api.streamingContext.sendRequest(StreamingRequest.unsubscribeCandle(instrument.figi, candleInterval))
     }
-//    OkHttpOpenApi(token, sandboxMode).use { api ->
-//        if (api.isSandboxMode) {
-//            api.sandboxContext.performRegistration(SandboxRegisterRequest()).join()
-//        }
-////        launch {
-////            logger.info("Run events consumer")
-////            api.streamingContext.asFlow().collect { event ->
-////                println("Got event: $event")
-////            }
-////        }
-//        val currentOrders = api.ordersContext.getOrders(null).join()
-//        logger.info("Current orders: $currentOrders")
-//        val currentPositions = api.portfolioContext.getPortfolio(null).join()
-//        logger.info("Current positions: $currentPositions")
-//
-//        val instrumentsList = api.marketContext.searchMarketInstrumentsByTicker(ticker).join()
-//        val instrument = instrumentsList.instruments.stream().findFirst().get()
-//
-//        val portfolioCurrencies = api.portfolioContext.getPortfolioCurrencies(null).join()
-//        logger.info("Portfolio currency: $portfolioCurrencies")
-//
-//        api.streamingContext.sendRequest(StreamingRequest.subscribeCandle(instrument.figi, candleInterval))
-//    }
 }
